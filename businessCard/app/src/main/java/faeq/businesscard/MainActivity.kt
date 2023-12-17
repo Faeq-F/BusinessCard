513773ae8243a15a5d0eefb7fdee84d4c6e03407
Faeq <faeqfaisal@hotmail.co.uk>
2023-12-17 15:45:04 +0000

    Updated profile image and fonts for text

--- businessCard/app/src/main/java/faeq/businesscard/MainActivity.kt
+++ businessCard/app/src/main/java/faeq/businesscard/MainActivity.kt
@@ -1,15 +1,21 @@
 package faeq.businesscard
 
+import android.content.Intent
+import android.database.Cursor
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.absoluteOffset
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Email
 import androidx.compose.material.icons.rounded.Person
@@ -22,7 +28,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontFamily
@@ -34,6 +39,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import faeq.businesscard.ui.theme.BusinessCardTheme
+
 
 class MainActivity : ComponentActivity() {
   
@@ -62,7 +68,7 @@ fun View(){
     stringResource(R.string.name),
     stringResource(R.string.job_title),
     stringResource(R.string.name_pronunciation),
-    stringResource(R.string.phone_number),
+    stringResource(getPhoneNumber()),
     stringResource(R.string.email),
     Modifier.fillMaxSize()
   )
@@ -82,29 +88,51 @@ fun NameSection(name : String, title:String, modifier : Modifier = Modifier){
         fontProvider = provider
       )
     )
+    val jobTitleFontFamily = FontFamily(
+      Font(
+        googleFont = GoogleFont("Baloo Bhaijaan 2"),
+        weight = FontWeight.W500,
+        fontProvider = provider
+      )
+    )
     Image(
-    contentDescription="Android logo",
-      painter = painterResource(id = R.drawable.ic_launcher_foreground),
-      modifier = Modifier.height(100.dp),
-      contentScale = ContentScale.FillBounds
+    contentDescription="Fyke Profile Image",
+      painter = painterResource(id = R.drawable.transparent_profile_pic),
+      modifier = Modifier
+        .height(180.dp)
+        .width(150.dp)
     )
     Text(
       text = name,
       fontSize = 50.sp,
       fontFamily = nameFontFamily,
-      modifier = modifier
+      modifier = modifier.padding(top=16.dp)
     )
     Text(
       text = title,
       fontSize = 17.sp,
+      fontFamily = jobTitleFontFamily,
       modifier = modifier
     )
   }
 }
 
+
+fun getPhoneNumber(): Int {
+  return R.string.phone_number
+}
+
 @Composable
 fun ExtraDetailsSection(pronunciation:String, number:String, email:String, modifier : Modifier = Modifier){
-Column(verticalArrangement = Arrangement.Bottom){
+  val detailsFontFamily = FontFamily(
+    Font(
+      googleFont = GoogleFont("Urbanist"),
+      weight = FontWeight.W300,
+      fontProvider = provider
+    )
+  )
+  
+Column(modifier = Modifier.absoluteOffset(0.dp,70.dp)){
   Row (modifier = Modifier.padding(bottom=10.dp)) {
     Icon(
       Icons.Rounded.Person,
@@ -112,6 +140,7 @@ Column(verticalArrangement = Arrangement.Bottom){
     )
     Text(
       text = pronunciation,
+      fontFamily = detailsFontFamily,
       modifier = Modifier.padding(start=15.dp)
     )
   }
@@ -120,11 +149,11 @@ Column(verticalArrangement = Arrangement.Bottom){
       Icons.Rounded.Phone,
       contentDescription = "Phone number Icon"
     )
-    Text(text = number,modifier = Modifier.padding(start=15.dp))
+    Text(text = number,fontFamily = detailsFontFamily,modifier = Modifier.padding(start=15.dp))
   }
   Row{
     Icon(Icons.Rounded.Email, contentDescription = "Email Icon")
-    Text(text = email,modifier = Modifier.padding(start=15.dp))
+    Text(text = email,fontFamily = detailsFontFamily,modifier = Modifier.padding(start=15.dp))
   }
 }
 }
@@ -136,7 +165,8 @@ fun BusinessCard(name : String, title:String, pronunciation:String, number:Strin
     color = Color.hsl(270F,0.8F,0.85F),
     modifier = modifier
   ){
-    Column (verticalArrangement = Arrangement.SpaceAround, horizontalAlignment = Alignment.CenterHorizontally) {
+    Column (verticalArrangement = Arrangement.SpaceAround, horizontalAlignment = Alignment
+      .CenterHorizontally) {
       NameSection(name, title)
       ExtraDetailsSection(pronunciation, number, email)
     }
